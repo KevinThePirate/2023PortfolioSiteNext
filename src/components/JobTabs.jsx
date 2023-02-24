37b96c957e6d487b3aa0a3012d9c833253ced9f6
Kevin <smithkevin1100@gmail.com>
2023-02-24 14:39:01 +0000

    updated job tab and populated with info

--- src/components/JobTabs.jsx
+++ src/components/JobTabs.jsx
@@ -14,7 +14,7 @@ export default function App() {
               key={item.label}
               className={item === selectedTab ? "selected" : ""}
               onClick={() => setSelectedTab(item)}>
-              {`${item.icon} ${item.label}`}
+              {`${item.label}`}
               {item === selectedTab ? (
                 <motion.div className="underline" layoutId="underline" />
               ) : null}
@@ -30,7 +30,17 @@ export default function App() {
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: -10, opacity: 0 }}
             transition={{ duration: 0.2 }}>
-            {selectedTab ? selectedTab.icon : "😋"}
+            {selectedTab ? (
+              <div>
+                <h4>{selectedTab.label}</h4>
+                <p>
+                  <b>{selectedTab.timeline}</b>
+                </p>
+                <p>{selectedTab.desc}</p>
+              </div>
+            ) : (
+              "😋"
+            )}
           </motion.div>
         </AnimatePresence>
       </main>
--- src/components/ingredients.jsx
+++ src/components/ingredients.jsx
@@ -4,13 +4,41 @@ export interface Ingredient {
 }
 
 export const allIngredients = [
-  { icon: "🍅", label: "MyFutprint Entertainment" },
-  { icon: "🥬", label: "MyFutprint Entertainment" },
-  { icon: "🧀", label: "MyFutprint Entertainment" },
-  { icon: "🥕", label: "Carrot" },
-  { icon: "🍌", label: "Banana" },
-  { icon: "🫐", label: "Blueberries" },
-  { icon: "🥂", label: "Champers?" },
+  {
+    label: "Motley Magazine",
+    timeline: "Sep '22 - Apr '23 ",
+    desc: "Graphic and Print Designer for six-print monthly print issues",
+  },
+  {
+    label: "Cork City Libraries",
+    timeline: "Dec ’19 - Mar ‘23",
+    desc: "Contracted to run another round of classes in my 'Fundamentals of Web Design' course by the Grand Parade, Hollyhill, Mayfield, Tory Top and Blackpool libraries",
+  },
+  {
+    label: "Nimbus Research Centre",
+    timeline: "Jun ‘22 – Aug ‘22",
+    desc: "User Experience Design intern working on products in a variety of industries",
+  },
+  {
+    label: "Freelance Web Design",
+    timeline: "May ’17 - Present",
+    desc: "Web design and implementation for various businesses such as TK Fitness, Sothic Biosciences, etc.",
+  },
+  {
+    label: "Ashton Secondary School",
+    timeline: "Aug ’19 – Oct ‘21 ",
+    desc: "Designed, wrote, and delivered a ten-week class entitled “Intro to Comic Production” for the Ashton Adult Education Program ",
+  },
+  {
+    label: "MyFutprint Entertainment, LLC",
+    timeline: "Jun ‘22 – Aug ‘22",
+    desc: "Started as a European Marketing intern and worked my way onto the editing team, helping to manage teams of creatives producing comic books for an international audience of hundreds of thousands. Initially brought on to work as a talent acquisition assistant and as an editor for internationally published graphic novels and magazines, then to manage that editing team before finally transitioning into my role as the European Marketing Intern where I researched and focused on expanding the company’s outreach and brand awareness in Europe along with working with the global marketing team. As part of this initiative, I lead events and brokered partnership, namely a company appearance at Ireland’s largest Japanese culture convention. Having worked well with the global team during my internship, I was brought onto the staff team to continue my work to bring our company to Europe. After showing my dedication and my ability to organise the production of such a project, I was appointed to spearhead our newest digital magazine Fan Art Friday released and read on our app by the thousands of people who have downloaded it. Lastly, I transitioned my role from before as an Editor & Producer into a part-time role to ensure I could uphold the quality of my work while on placement for college",
+  },
+  {
+    label: "DePuy Synthes – Johnson & Johnson",
+    timeline: "Jan ‘21 - Aug ‘21",
+    desc: "Role: Communications Co-Op. Acted as the sole in-house designer and video producer for the global DePuy Synthes company, under Johnson & Johnson global. This involved Web Design, Graphic Design, Video Editing & Motion Graphic Design for a global audience and writing a comprehensive, 60+ page 'Communications Playbook' teaching future team members how to interact with all of our various technical systems.  Key roles included being the creative director for the DPS STEM Academy's 'Mission To Mars' Summer Camp. This role involved designing all the assets for the camp, writing, designing, and editing a 4-episode mini-series to go along with the lessons, many of which I helped to plan and facilitate with the children myself.",
+  },
 ];
 
 const [tomato, lettuce, cheese] = allIngredients;
